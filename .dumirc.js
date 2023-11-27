b816edaadba1663c6e88d3c2a1f0dcad53ac858e
Duckyo <duckyo0308@126.com>
2023-11-27 19:31:33 +0800

    feat(duckyo): :sparkles: 添加 duckyo 初始化

---
+++ .dumirc.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var dumi_1 = require("dumi");
+exports.default = (0, dumi_1.defineConfig)({
+    themeConfig: {
+        name: '',
+    },
+    base: '/duckyo/',
+    publicPath: '/duckyo/',
+});
